c82008d522c02cfdd6f1214762a5a4d6e5c746cb
Shaked Hagag <shaked.hagag@openweb.com>
2024-05-18 16:27:36 +0300

    changed import

--- utils/screenshots.ts
+++ utils/screenshots.ts
@@ -1,11 +1,7 @@
 const importAll = (context: __WebpackModuleApi.RequireContext) =>
   context.keys().map(context);
 const screenshots = importAll(
-  require.context(
-    "../../frontend/public/images/screenshots/",
-    false,
-    /\.(png|jpe?g|svg)$/,
-  ),
+  require.context("@/public/images/screenshots/", false, /\.(png|jpe?g|svg)$/),
 );
 
 export default screenshots;
